e6c8ac2d2d7616cd9c20bb3733e3b2766b16b173
50m-regent <len.hirata@gmail.com>
2024-11-10 16:59:30 +0900

    pre-commit設定

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 import transformers
+
+
+def main() -> None:
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+        "google-bert/bert-base-uncased"
+    )
+
+    print(model)
