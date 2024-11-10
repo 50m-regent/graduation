4fc2ac5ebb96c5d3eab5cafe30410c12d85d56b0
50m-regent <len.hirata@gmail.com>
2024-11-10 20:00:29 +0900

    ベンチマーク測定

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,9 @@
 import datasets
-from transformers import AutoModel
+import torch
+from sklearn import metrics
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+from transformers import AutoModel, AutoTokenizer
 
 from utils import get_logger
 
@@ -7,7 +11,49 @@ from utils import get_logger
 logger = get_logger(__name__)
 
 
-def imdb_benchmark(model: AutoModel) -> None:
+def imdb_benchmark(model: AutoModel, tokenizer: AutoTokenizer) -> None:
     dataset = datasets.load_dataset("imdb")["test"]
 
     logger.info(dataset)
+
+    labels = dataset["label"]
+
+    dataset = tokenizer(
+        dataset["text"],
+        padding=True,
+        truncation=True,
+        return_tensors="pt",
+    )
+
+    predictions = []
+
+    with torch.no_grad(), logging_redirect_tqdm(loggers=[logger]):
+        for input_ids, token_type_ids, attention_mask in tqdm(
+            zip(
+                dataset["input_ids"],
+                dataset["token_type_ids"],
+                dataset["attention_mask"],
+            ),
+            total=len(dataset["input_ids"]),
+        ):
+            prediction = (
+                model(
+                    input_ids.unsqueeze(dim=0),
+                    token_type_ids.unsqueeze(dim=0),
+                    attention_mask.unsqueeze(dim=0),
+                )
+                .logits.argmax()
+                .item()
+            )
+
+            predictions.append(prediction)
+
+    accuracy = metrics.accuracy_score(labels, predictions)
+    f1 = metrics.f1_score(labels, predictions).item()
+    precision = metrics.precision_score(labels, predictions).item()
+    recall = metrics.recall_score(labels, predictions).item()
+
+    logger.info(f"{accuracy=}")
+    logger.info(f"{f1=}")
+    logger.info(f"{precision=}")
+    logger.info(f"{recall=}")
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-import transformers
-from transformers.models.bert.modeling_bert import BertSdpaSelfAttention
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 from benchmark import imdb_benchmark
 from utils import get_logger
@@ -9,19 +8,22 @@ logger = get_logger(__name__)
 
 
 def main() -> None:
-    model = transformers.AutoModelForSequenceClassification.from_pretrained(
+    model = AutoModelForSequenceClassification.from_pretrained(
         "google-bert/bert-base-uncased"
     )
+    tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
 
+    """
     for _, module in model.named_modules():
         if not isinstance(module, BertSdpaSelfAttention):
             continue
 
         module.position_embedding_type = None
+    """
 
     logger.info(model)
 
-    imdb_benchmark(model)
+    imdb_benchmark(model, tokenizer)
 
 
 if __name__ == "__main__":
